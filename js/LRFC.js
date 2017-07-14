86538c13289e7a06d466fffb9da74a55f930087e
Emmett Peters <ejp8611@gmail.com>
2017-07-14 16:46:30 -0500

    updates to LRFC

--- js/LRFC.js
+++ js/LRFC.js
@@ -6,3 +6,13 @@ $(window).scroll(function() {
       $('#dropdown').slideDown();
      }
  });
+var i=0;
+
+$("#testButton").click(function(){
+	i++
+	$('#contentSpacer').append("<div class='lottoRaffle'>Lotto " + i +"</div>") ;
+});
+
+$("#testButton2").click(function(){
+	$('#contentSpacer').children().first().remove();
+});
